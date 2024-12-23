3405c756c09c2873ee697d90db1107504d409fa3
Kashaf Shaikh <billushaikhshaikh@gmail.com>
2024-12-23 05:38:00 +0500

    updated

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -2,12 +2,12 @@ export interface simpleBlogCard{
     title: string;
     smallDescription: string;
     currentSlug: string;
-    titleImage: any
+    titleImage: string
 }
 
 export interface fullBlog{
     currentSlug: string;
     title: string;
-    content: any;
-    titleImage: any
+    content: Array<any>;
+    titleImage: string
 }
