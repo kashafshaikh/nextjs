05a6a4ccf8b6e744eb5319a87b8090418c35f3e3
Kashaf Shaikh <billushaikhshaikh@gmail.com>
2024-12-25 04:21:20 +0500

    updated

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,9 +2,16 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
 
+// export const client = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+// })
+
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  projectId: "zatwz31j",  // Replace with your Sanity project ID
+  dataset: "production",         // Your dataset name
+  apiVersion: "2023-05-03",      // Specify the API version you're using
+  useCdn: true,                  // Use CDN for faster responses (true/false)
+});
